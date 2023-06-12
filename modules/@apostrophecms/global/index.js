679a6e4f51a36b32521d816580dcbafdb292b1c7
snoeretje1 <remcosnoeren@rs2e.nl>
2023-06-12 12:46:09 +0200

    NAW-moudle global

--- modules/@apostrophecms/global/index.js
+++ modules/@apostrophecms/global/index.js
@@ -1,6 +1,30 @@
 module.exports = {
     fields: {
       add: {
+        Naambedrijf:{
+          type:'string',
+          label:'Naam bedrijf'
+        },
+        straathuisnummer:{
+          type:'string',
+          label:'Straat+Huisnummer'
+        },
+        telefoonnummer:{
+          type:'string',
+          label:'telefoon'
+        },
+        email:{
+          type:'string',
+          label:'email'
+        },
+        kvk:{
+          type:'string',
+          label:'kvk'
+        },
+        iban:{
+          type:'string',
+          label:'iban'
+        },
         githubUrl: {
           type: 'url',
           label: 'Github organization url'
@@ -8,6 +32,10 @@ module.exports = {
         
       },
       group: {
+        naw:{
+          label:'Naw',
+          fields: ['Naambedrijf','straathuisnummer','telefoonnummer','email','kvk','iban']
+        },
         basics: {
           label: 'Basics',
           fields: ['githubUrl']
